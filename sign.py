38e9887c707aa1af8f7552a344134b33de716358
Ayan gantayat <ayangantayat095@gmail.com>
2024-01-25 21:42:04 +0530

    initial commit

---
+++ sign.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("My Streamlit App")
+
+# Add a link to the YouTube video
+st.markdown('<a href="https://www.youtube.com/watch?v=xvFZjo5PgG0" target="_blank">Watch the YouTube Video</a>', unsafe_allow_html=True)
